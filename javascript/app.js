3e74420098aab46a9b3ba9dbac69bcea8ff10836
Ian Kiplagat <ianjkiplagat@gmail.com>
2021-02-17 20:15:39 +0300

    add function akan

--- javascript/app.js
+++ javascript/app.js
@@ -1,2 +1,7 @@
-alert("connected");
-
+function akan(){
+    var birthDate=document.getElementById('date').value;
+    var gender=document.getElementById('gender').value;
+    birthDate.toString();
+    if (!birthDate) {
+      alert("You entered an invalid date");
+    }
