d996b2e7bf021c469f3590de0ab51356b5226a5a
Ian Kiplagat <ianjkiplagat@gmail.com>
2021-02-18 12:35:44 +0300

    modify generator formula

--- javascript/app.js
+++ javascript/app.js
@@ -5,13 +5,11 @@ function akan() {
   if (!birthDate) {
     alert("You entered an invalid date");
   }
-  var centuryYear;
-  var birthYear;
+
   var birthMonth;
   var birthDay;
   var year;
-  centuryYear = parseInt(birthDate.slice(0, 2));
-  birthYear = parseInt(birthDate.slice(2, 4));
+
   birthMonth = parseInt(birthDate.slice(5, 7));
   birthDay = parseInt(birthDate.slice(8, 10));
   year = parseInt(birthDate.slice(0, 4));
@@ -32,41 +30,17 @@ function akan() {
   ];
   maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  var dayOfTheWeek;
-  var day;
-  dayOfTheWeek =
-    (centuryYear / 4 -
-      2 * centuryYear -
-      1 +
-      (5 * birthYear) / 4 +
-      (26 * (birthMonth + 1)) / 10 +
-      birthDay) %
-    7;
-  if (dayOfTheWeek < 0) {
-    day = Math.round(dayOfTheWeek) + 7;
-  } else {
-    day = Math.trunc(dayOfTheWeek);
-  }
-  if (year < 1920 || (year > 1920 && year < 1979)) {
-    day = day - 1;
-  }
-  if (year >= 1980 && year < 2000) {
-    day = day - 1;
-  }
-  if (year === 1979) {
-    day = day + 6;
-  }
-  if (year === 1990) {
-    day = day + 7;
-  }
+
+  let getDate = new Date(year, birthMonth - 1, birthDay);
+  let dayNum = getDate.getDay();
+  let dayName = weekDays[dayNum];
+  console.log(dayName);
+
   var akanName;
-  var dayName;
   if (gender === "male") {
-    akanName = maleNames[day];
-    dayName = weekDays[day];
+    akanName = maleNames[dayNum];
   } else {
     akanName = femaleNames[day];
-    dayName = weekDays[day];
   }
   document.getElementById("output").innerHTML =
     "Your akhan name is: " + akanName + " meaning you were born on " + dayName;
